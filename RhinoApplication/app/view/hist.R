d648cc2c3179211cf6db67e2d29b9785eb46cf33
Robert Zell <robert.zell@aimpointdigital.com>
2025-03-20 09:20:01 -0400

    Update hist.R

--- RhinoApplication/app/view/hist.R
+++ RhinoApplication/app/view/hist.R
@@ -1,6 +1,6 @@
 # app/view/hist.R
 box::use(
-  shiny[h3, moduleServer, NS, tagList, plotOutput, renderPlot],
+  shiny[h3, moduleServer, NS, tagList, plotOutput, renderPlot, selectInput],
   ggplot2[ggplot, aes, geom_histogram, theme_minimal, ggtitle],
   stats[rnorm]
 )
@@ -10,6 +10,8 @@ ui <- function(id) {
   ns <- NS(id)
   tagList(
     h3("Histogram"),
+    selectInput(ns("fill_color"), "Choose Fill Color",
+                 choices = c("Red"="red", "Blue"="blue", "Green"="green")),
     plotOutput(ns("hist"))
   )
 }
@@ -22,7 +24,7 @@ server <- function(id) {
       df <- data.frame(x = rnorm(100))
       
       ggplot(df, aes(x)) +
-        geom_histogram(binwidth = 0.5, fill = "skyblue", color = "black") +
+        geom_histogram(binwidth = 0.5, fill = input$fill_color, color = "black") +
         theme_minimal() +
         ggtitle("Distribution of X Values")
     })
